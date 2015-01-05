5dedef8c26b1020c786a6610d37a4484451419f6
unknown <scarda01@CSPC048.dcs.bbk.ac.uk>
2015-01-05 18:33:15 +0000

    Creating an object array. Implementing the method isEmpty()

---
+++ ArrayList.java
@@ -0,0 +1,47 @@
+package cw3;
+
+public class ArrayList implements List {
+	
+	public Object[] myarray;
+	
+	@Override
+	public boolean isEmpty() {
+		if(myarray.length > 0){
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ReturnObject get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReturnObject remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReturnObject add(int index, Object item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReturnObject add(Object item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
