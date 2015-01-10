cd2be74e75ba661558888257b947025afb43132a
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2015-01-10 23:57:40 +0000

    implementing the main test method (using the ErrorMessage enum). writing down a few tests.

--- Tests.java
+++ Tests.java
@@ -7,10 +7,19 @@ public class Tests {
 		//System.out.print(myarray[2]); //testing if returns null with no errors
 		
 		ArrayList mylist = new ArrayList();
+		int m = 5;
+		String stringTest = "test";
+		Integer integTest = null;
+		
 		if(!mylist.isEmpty()) System.out.print("The isEmpty method did not pass the test");
+		test(mylist.add(-2, m).getError(),ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		test(mylist.add(m).getError(),ErrorMessage.);
+		test(mylist.add(2, stringTest).getError(),ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		test(mylist.add(2, integTest).getError(),ErrorMessage.INVALID_ARGUMENT);
 	}
 	
-	public boolean test(String item1, String item2)
+	
+	private static boolean test(String item1, String item2)
 	{
 		if(item1.equals(item2)) {
 			return true;
@@ -18,7 +27,7 @@ public class Tests {
 		return false;
 	}
 	
-	public boolean test(int item1, int item2)
+	private static boolean test(int item1, int item2)
 	{
 		if(item1 == item2) {
 			return true;
@@ -26,6 +35,12 @@ public class Tests {
 		return false;
 	}
 	
+	private static boolean test(ErrorMessage errorGenerated, ErrorMessage errorExpected) {
+		if(errorGenerated.equals(errorExpected)) {
+			return true;
+		}
+		return false;
+	}
 	//ArrayList tests
 	
 }
