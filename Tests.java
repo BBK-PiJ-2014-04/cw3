9e1b308262f554c1dd22f18caadee9e4fdd240ab
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2015-01-10 23:37:55 +0000

    implementing the methods to test

--- Tests.java
+++ Tests.java
@@ -3,7 +3,29 @@ package cw3;
 public class Tests {
 	public static void main(String [] args)
 	{
-		Object[] myarray = new Object[100];
-		System.out.print(myarray[2]); //testing if returns null with no errors
+		//Object[] myarray = new Object[100];
+		//System.out.print(myarray[2]); //testing if returns null with no errors
+		
+		ArrayList mylist = new ArrayList();
+		if(!mylist.isEmpty()) System.out.print("The isEmpty method did not pass the test");
 	}
+	
+	public boolean test(String item1, String item2)
+	{
+		if(item1.equals(item2)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean test(int item1, int item2)
+	{
+		if(item1 == item2) {
+			return true;
+		}
+		return false;
+	}
+	
+	//ArrayList tests
+	
 }
