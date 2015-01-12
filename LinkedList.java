fccd63647d536d94e440bf7738a36aea5ae3db28
unknown <scarda01@CSPC049.dcs.bbk.ac.uk>
2015-01-12 18:54:47 +0000

    Initializing the linkedlist

---
+++ LinkedList.java
@@ -0,0 +1,43 @@
+package cw3;
+
+public class LinkedList implements List {
+	private Object Listdata;
+	private LinkedList pointer;
+	
+	
+	@Override
+	public boolean isEmpty() {
+		return (pointer == null) ? false : true;		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ReturnObject get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReturnObject remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReturnObject add(int index, Object item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReturnObject add(Object item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
