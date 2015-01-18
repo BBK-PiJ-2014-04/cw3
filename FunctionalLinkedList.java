d3c049165365b2d7560bbd5c852e53d651b832f8
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2015-01-18 22:04:06 +0000

    The rest method is exactly the same as the ArrayList one.

--- FunctionalLinkedList.java
+++ FunctionalLinkedList.java
@@ -8,14 +8,17 @@ public class FunctionalLinkedList extends LinkedList implements FunctionalList {
 			return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
 		}
 		else {
-			return new ReturnObjectImpl(this.get(0));
+			return new ReturnObjectImpl(this.get(0)); // I could have used "pointer.ListData" but in order to do it I should have changed pointer from private to protected. I rather use the get method.
 		}
 	}
 
 	@Override
 	public FunctionalList rest() {
-		// TODO Auto-generated method stub
-		return null;
+		FunctionalLinkedList mynewList = new FunctionalLinkedList();
+		for(int i = 1; i < this.size(); i++) {
+			mynewList.add(this.get(i).getReturnValue());
+		}
+		return mynewList;
 	}
 	
 }
