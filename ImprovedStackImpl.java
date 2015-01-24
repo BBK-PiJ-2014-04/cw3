26bb9b5e1ed6de6fa416fb0a759566ecd68f8c9d
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2015-01-24 17:31:10 +0000

    Nothing is needed if the object searched on the remove method is null

--- ImprovedStackImpl.java
+++ ImprovedStackImpl.java
@@ -57,13 +57,15 @@ public class ImprovedStackImpl implements ImprovedStack {
 
 	@Override
 	public void remove(Object object) {
-		ReturnObject currentElement = null;
-		for(int i = 0; i < this.size(); i++) {
-			currentElement = this.mystack.get(i);
-			if(!currentElement.hasError() //We shouldn't have any error, but better to be safe than sorry
-					&& (object.equals(currentElement.getReturnValue())) 
-					) {
-				this.mystack.remove(i);
+		if(object != null) {
+			ReturnObject currentElement = null;
+			for(int i = 0; i < this.size(); i++) {
+				currentElement = this.mystack.get(i);
+				if(!currentElement.hasError() //We shouldn't have any error, but better to be safe than sorry
+						&& (object.equals(currentElement.getReturnValue())) 
+						) {
+					this.mystack.remove(i);
+				}
 			}
 		}
 	}
